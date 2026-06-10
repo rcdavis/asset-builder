009fa3f34a1d0f820d43ecf77cf9bc5c42c89522
Ren Davis <rcdavisgd@gmail.com>
2026-06-10 03:54:40 -0400

    Updated exception comments

--- AssetBuilder/src/Main.cpp
+++ AssetBuilder/src/Main.cpp
@@ -6,16 +6,17 @@
 int main(int argc, char** argv) {
 	Log::Init();
 
-	CLI::App app("Asset Builder for building and converting game assets");
+	CLI::App app("Program for building and converting game assets");
 	app.set_version_flag("-V,--version", "1.0.0");
 
 	try {
 		app.parse(argc, argv);
 	} catch (const CLI::ParseError& e) {
-		// Exit code will be 0 if using --help or --version. This isn't a real error.
+		// Exception will be thrown with exit code 0 if using --help or --version. This isn't a real error.
 		if (e.get_exit_code() != 0) {
 			LOG_ERROR("Error parsing command line arguments: {}", e.what());
 		}
+
 		return app.exit(e);
 	}
 
