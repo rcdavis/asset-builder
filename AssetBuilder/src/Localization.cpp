6408c3994c33732bc5964170f273919d710b3ad2
Ren Davis <rcdavisgd@gmail.com>
2026-06-11 08:16:08 -0400

    Added namespace for Localization code (#4)

--- AssetBuilder/src/Localization.cpp
+++ AssetBuilder/src/Localization.cpp
@@ -11,269 +11,271 @@
 #include "simdjson.h"
 #include "fmt/format.h"
 
-enum PluralCategory : uint8_t {
-	One,
-	Other,
-	Count
-};
+namespace Localization {
+	enum PluralCategory : uint8_t {
+		One,
+		Other,
+		Count
+	};
 
-struct LocalizationEntry {
-	uint32_t firstForm = 0;
-	uint32_t formCount = 0;
-};
+	struct Entry {
+		uint32_t firstForm = 0;
+		uint32_t formCount = 0;
+	};
 
-struct LocalizationForm {
-	uint32_t category = 0;
-	uint32_t offset = 0;
-	uint32_t length = 0;
-};
+	struct Form {
+		uint32_t category = 0;
+		uint32_t offset = 0;
+		uint32_t length = 0;
+	};
 
-struct ParsedLocalizationForm {
-	std::string text;
-	PluralCategory pluralCategory = PluralCategory::Other;
-};
+	struct ParsedForm {
+		std::string text;
+		PluralCategory pluralCategory = PluralCategory::Other;
+	};
 
-struct ParsedLocalizationEntry {
-	std::string key;
-	std::vector<ParsedLocalizationForm> forms;
-};
+	struct ParsedEntry {
+		std::string key;
+		std::vector<ParsedForm> forms;
+	};
 
-struct LocalizationBinHeader {
-	char magic[4] {};
-	uint32_t version = 0;
-	uint32_t entryCount = 0;
-	uint32_t formCount = 0;
-	uint32_t stringPoolSize = 0;
-};
+	struct BinHeader {
+		char magic[4] {};
+		uint32_t version = 0;
+		uint32_t entryCount = 0;
+		uint32_t formCount = 0;
+		uint32_t stringPoolSize = 0;
+	};
 
-static LocalizationEntry* s_entries = nullptr;
-static LocalizationForm* s_forms = nullptr;
-static char* s_stringData = nullptr;
+	static Entry* s_entries = nullptr;
+	static Form* s_forms = nullptr;
+	static char* s_stringData = nullptr;
 
-static uint32_t s_entryCount = 0;
-static uint32_t s_formCount = 0;
-static uint32_t s_stringDataSize = 0;
+	static uint32_t s_entryCount = 0;
+	static uint32_t s_formCount = 0;
+	static uint32_t s_stringDataSize = 0;
 
-static bool Localization_ParseFile(const char* filePath, std::vector<ParsedLocalizationEntry>& entries);
-static bool Localization_ExportFile(const char* filePath, const std::vector<ParsedLocalizationEntry>& entries);
+	static bool ParseFile(const char* filePath, std::vector<ParsedEntry>& entries);
+	static bool ExportFile(const char* filePath, const std::vector<ParsedEntry>& entries);
 
-// TODO: Should handle multiple languages.
-static PluralCategory ConvertToPluralCategory(uint32_t count) {
-	if (count == 1)
-		return PluralCategory::One;
+	// TODO: Should handle multiple languages.
+	static PluralCategory ConvertToPluralCategory(uint32_t count) {
+		if (count == 1)
+			return PluralCategory::One;
 
-	return PluralCategory::Other;
-}
-
-bool Localization_CompileStrings(const char* inputFile, const char* outputFile) {
-	std::vector<ParsedLocalizationEntry> parsedEntries;
-	if (!Localization_ParseFile(inputFile, parsedEntries)) {
-		LOG_ERROR("Failed to parse localization file: \"{}\"", inputFile);
-		return false;
+		return PluralCategory::Other;
 	}
 
-	if (!Localization_ExportFile(outputFile, parsedEntries)) {
-		LOG_ERROR("Failed to export localization file \"{}\" to \"{}\"", inputFile, outputFile);
-		return false;
-	}
-
-	return true;
-}
-
-bool Localization_Load(const char* filePath) {
-	std::ifstream file(filePath, std::ios::binary);
-	if (!file) {
-		LOG_ERROR("Failed to open localization file \"{}\"", filePath);
-		return false;
-	}
-
-	LocalizationBinHeader header;
-	file.read((char*)&header, sizeof(LocalizationBinHeader));
-
-	if (memcmp(header.magic, "LOCB", 4) != 0) {
-		LOG_ERROR("Localization file \"{}\" doesn't have a valid signature", filePath);
-		return false;
-	}
-
-	Localization_Destroy();
-
-	s_entries = new LocalizationEntry[header.entryCount];
-	s_entryCount = header.entryCount;
-
-	file.read((char*)s_entries, header.entryCount * sizeof(LocalizationEntry));
-
-	s_forms = new LocalizationForm[header.formCount];
-	s_formCount = header.formCount;
-
-	file.read((char*)s_forms, header.formCount * sizeof(LocalizationForm));
-
-	s_stringData = new char[header.stringPoolSize];
-	s_stringDataSize = header.stringPoolSize;
-
-	file.read(s_stringData, header.stringPoolSize);
-
-	return true;
-}
-
-void Localization_Destroy() {
-	delete[] s_entries;
-	s_entries = nullptr;
-
-	delete[] s_forms;
-	s_forms = nullptr;
-
-	delete[] s_stringData;
-	s_stringData = nullptr;
-
-	s_entryCount = 0;
-	s_formCount = 0;
-	s_stringDataSize = 0;
-}
-
-const char* Localization_GetString(uint32_t id) {
-	assert(s_entries != nullptr && "Localization entries not loaded");
-	assert(s_forms != nullptr && "Localization forms not loaded");
-	assert(s_stringData != nullptr && "Localization string data not loaded");
-	assert(id < s_entryCount && "Invalid localization ID");
-
-	const LocalizationEntry& entry = s_entries[id];
-	const LocalizationForm& form = s_forms[entry.firstForm];
-
-	return s_stringData + form.offset;
-}
-
-std::string Localization_GetPlural(uint32_t id, uint32_t count) {
-	assert(s_entries != nullptr && "Localization entries not loaded");
-	assert(s_forms != nullptr && "Localization forms not loaded");
-	assert(s_stringData != nullptr && "Localization string data not loaded");
-	assert(id < s_entryCount && "Invalid localization ID");
-
-	const PluralCategory category = ConvertToPluralCategory(count);
-
-	const LocalizationEntry& entry = s_entries[id];
-
-	for (uint32_t i = 0; i < entry.formCount; ++i) {
-		const LocalizationForm& form = s_forms[entry.firstForm + i];
-
-		if (form.category == category) {
-			const char* const str = s_stringData + form.offset;
-			return fmt::format(fmt::runtime(str), fmt::arg("count", count));
-		}
-	}
-
-	return {};
-}
-
-static bool Localization_ParseFile(const char* filePath, std::vector<ParsedLocalizationEntry>& entries) {
-	simdjson::ondemand::parser parser;
-	auto json = simdjson::padded_string::load(filePath);
-	auto doc = parser.iterate(json);
-
-	for (auto field : doc.get_object()) {
-		std::string_view key = field.unescaped_key();
-		auto value = field.value();
-
-		switch (value.type()) {
-		// Singular text entry
-		case simdjson::ondemand::json_type::string: {
-			ParsedLocalizationEntry entry;
-			entry.key = std::string(key);
-			const std::string_view text = value.get_string();
-
-			entry.forms.push_back({
-				.text = std::string(text),
-				.pluralCategory = PluralCategory::Other,
-			});
-
-			entries.push_back(std::move(entry));
-		}
-		break;
-
-		// Plural text entry
-		case simdjson::ondemand::json_type::object: {
-			ParsedLocalizationEntry entry;
-			entry.key = std::string(key);
-
-			for (auto pluralField : value.get_object()) {
-				std::string_view pluralKey = pluralField.unescaped_key();
-				auto pluralValue = pluralField.value();
-
-				if (pluralValue.type() != simdjson::ondemand::json_type::string) {
-					LOG_ERROR("Invalid plural form value for key '{}.{}'", key, pluralKey);
-					return false;
-				}
-
-				std::string_view text = pluralValue.get_string();
-
-				ParsedLocalizationForm form;
-				form.text = std::string(text);
-
-				if (pluralKey == "one") {
-					form.pluralCategory = PluralCategory::One;
-				} else if (pluralKey == "other") {
-					form.pluralCategory = PluralCategory::Other;
-				} else {
-					LOG_ERROR("Unsupported plural category '{}' for key '{}'", pluralKey, key);
-					return false;
-				}
-
-				entry.forms.push_back(std::move(form));
-			}
-
-			entries.push_back(std::move(entry));
-		}
-		break;
-
-		default:
-			LOG_ERROR("Unsupported JSON value type for key '{}'", key);
+	bool CompileStrings(const char* inputFile, const char* outputFile) {
+		std::vector<ParsedEntry> parsedEntries;
+		if (!ParseFile(inputFile, parsedEntries)) {
+			LOG_ERROR("Failed to parse localization file: \"{}\"", inputFile);
 			return false;
 		}
-	}
 
-	return true;
-}
-
-static bool Localization_ExportFile(const char* filePath, const std::vector<ParsedLocalizationEntry>& entries) {
-	std::vector<LocalizationEntry> locEntries;
-	std::vector<LocalizationForm> locForms;
-	std::vector<char> stringPool;
-
-	for (const auto& parsedEntry : entries) {
-		LocalizationEntry entry;
-		entry.firstForm = (uint32_t)std::size(locForms);
-		entry.formCount = (uint32_t)std::size(parsedEntry.forms);
-
-		for (const auto& parsedForm : parsedEntry.forms) {
-			LocalizationForm form;
-			form.category = (uint32_t)parsedForm.pluralCategory;
-			form.offset = (uint32_t)std::size(stringPool);
-			form.length = (uint32_t)std::size(parsedForm.text);
-
-			stringPool.insert(std::end(stringPool), std::cbegin(parsedForm.text), std::cend(parsedForm.text));
-			stringPool.push_back('\0');
-
-			locForms.push_back(std::move(form));
+		if (!ExportFile(outputFile, parsedEntries)) {
+			LOG_ERROR("Failed to export localization file \"{}\" to \"{}\"", inputFile, outputFile);
+			return false;
 		}
 
-		locEntries.push_back(std::move(entry));
+		return true;
 	}
 
-	std::ofstream file(filePath, std::ios::binary);
-	if (!file) {
-		LOG_ERROR("Failed to create file \"{}\"", filePath);
-		return false;
+	bool Load(const char* filePath) {
+		std::ifstream file(filePath, std::ios::binary);
+		if (!file) {
+			LOG_ERROR("Failed to open localization file \"{}\"", filePath);
+			return false;
+		}
+
+		BinHeader header;
+		file.read((char*)&header, sizeof(BinHeader));
+
+		if (memcmp(header.magic, "LOCB", 4) != 0) {
+			LOG_ERROR("Localization file \"{}\" doesn't have a valid signature", filePath);
+			return false;
+		}
+
+		Destroy();
+
+		s_entries = new Entry[header.entryCount];
+		s_entryCount = header.entryCount;
+
+		file.read((char*)s_entries, header.entryCount * sizeof(Entry));
+
+		s_forms = new Form[header.formCount];
+		s_formCount = header.formCount;
+
+		file.read((char*)s_forms, header.formCount * sizeof(Form));
+
+		s_stringData = new char[header.stringPoolSize];
+		s_stringDataSize = header.stringPoolSize;
+
+		file.read(s_stringData, header.stringPoolSize);
+
+		return true;
 	}
 
-	LocalizationBinHeader header;
-	memcpy(header.magic, "LOCB", 4);
-	header.entryCount = (uint32_t)std::size(locEntries);
-	header.formCount = (uint32_t)std::size(locForms);
-	header.stringPoolSize = (uint32_t)std::size(stringPool);
+	void Destroy() {
+		delete[] s_entries;
+		s_entries = nullptr;
 
-	file.write((char*)&header, sizeof(LocalizationBinHeader));
-	file.write((char*)std::data(locEntries), std::size(locEntries) * sizeof(LocalizationEntry));
-	file.write((char*)std::data(locForms), std::size(locForms) * sizeof(LocalizationForm));
-	file.write(std::data(stringPool), std::size(stringPool));
+		delete[] s_forms;
+		s_forms = nullptr;
 
-	return true;
+		delete[] s_stringData;
+		s_stringData = nullptr;
+
+		s_entryCount = 0;
+		s_formCount = 0;
+		s_stringDataSize = 0;
+	}
+
+	const char* GetString(uint32_t id) {
+		assert(s_entries != nullptr && "Localization entries not loaded");
+		assert(s_forms != nullptr && "Localization forms not loaded");
+		assert(s_stringData != nullptr && "Localization string data not loaded");
+		assert(id < s_entryCount && "Invalid localization ID");
+
+		const Entry& entry = s_entries[id];
+		const Form& form = s_forms[entry.firstForm];
+
+		return s_stringData + form.offset;
+	}
+
+	std::string GetPlural(uint32_t id, uint32_t count) {
+		assert(s_entries != nullptr && "Localization entries not loaded");
+		assert(s_forms != nullptr && "Localization forms not loaded");
+		assert(s_stringData != nullptr && "Localization string data not loaded");
+		assert(id < s_entryCount && "Invalid localization ID");
+
+		const PluralCategory category = ConvertToPluralCategory(count);
+
+		const Entry& entry = s_entries[id];
+
+		for (uint32_t i = 0; i < entry.formCount; ++i) {
+			const Form& form = s_forms[entry.firstForm + i];
+
+			if (form.category == category) {
+				const char* const str = s_stringData + form.offset;
+				return fmt::format(fmt::runtime(str), fmt::arg("count", count));
+			}
+		}
+
+		return {};
+	}
+
+	static bool ParseFile(const char* filePath, std::vector<ParsedEntry>& entries) {
+		simdjson::ondemand::parser parser;
+		auto json = simdjson::padded_string::load(filePath);
+		auto doc = parser.iterate(json);
+
+		for (auto field : doc.get_object()) {
+			std::string_view key = field.unescaped_key();
+			auto value = field.value();
+
+			switch (value.type()) {
+			// Singular text entry
+			case simdjson::ondemand::json_type::string: {
+				ParsedEntry entry;
+				entry.key = std::string(key);
+				const std::string_view text = value.get_string();
+
+				entry.forms.push_back({
+					.text = std::string(text),
+					.pluralCategory = PluralCategory::Other,
+				});
+
+				entries.push_back(std::move(entry));
+			}
+			break;
+
+			// Plural text entry
+			case simdjson::ondemand::json_type::object: {
+				ParsedEntry entry;
+				entry.key = std::string(key);
+
+				for (auto pluralField : value.get_object()) {
+					std::string_view pluralKey = pluralField.unescaped_key();
+					auto pluralValue = pluralField.value();
+
+					if (pluralValue.type() != simdjson::ondemand::json_type::string) {
+						LOG_ERROR("Invalid plural form value for key '{}.{}'", key, pluralKey);
+						return false;
+					}
+
+					std::string_view text = pluralValue.get_string();
+
+					ParsedForm form;
+					form.text = std::string(text);
+
+					if (pluralKey == "one") {
+						form.pluralCategory = PluralCategory::One;
+					} else if (pluralKey == "other") {
+						form.pluralCategory = PluralCategory::Other;
+					} else {
+						LOG_ERROR("Unsupported plural category '{}' for key '{}'", pluralKey, key);
+						return false;
+					}
+
+					entry.forms.push_back(std::move(form));
+				}
+
+				entries.push_back(std::move(entry));
+			}
+			break;
+
+			default:
+				LOG_ERROR("Unsupported JSON value type for key '{}'", key);
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	static bool ExportFile(const char* filePath, const std::vector<ParsedEntry>& entries) {
+		std::vector<Entry> locEntries;
+		std::vector<Form> locForms;
+		std::vector<char> stringPool;
+
+		for (const auto& parsedEntry : entries) {
+			Entry entry;
+			entry.firstForm = (uint32_t)std::size(locForms);
+			entry.formCount = (uint32_t)std::size(parsedEntry.forms);
+
+			for (const auto& parsedForm : parsedEntry.forms) {
+				Form form;
+				form.category = (uint32_t)parsedForm.pluralCategory;
+				form.offset = (uint32_t)std::size(stringPool);
+				form.length = (uint32_t)std::size(parsedForm.text);
+
+				stringPool.insert(std::end(stringPool), std::cbegin(parsedForm.text), std::cend(parsedForm.text));
+				stringPool.push_back('\0');
+
+				locForms.push_back(std::move(form));
+			}
+
+			locEntries.push_back(std::move(entry));
+		}
+
+		std::ofstream file(filePath, std::ios::binary);
+		if (!file) {
+			LOG_ERROR("Failed to create file \"{}\"", filePath);
+			return false;
+		}
+
+		BinHeader header;
+		memcpy(header.magic, "LOCB", 4);
+		header.entryCount = (uint32_t)std::size(locEntries);
+		header.formCount = (uint32_t)std::size(locForms);
+		header.stringPoolSize = (uint32_t)std::size(stringPool);
+
+		file.write((char*)&header, sizeof(BinHeader));
+		file.write((char*)std::data(locEntries), std::size(locEntries) * sizeof(Entry));
+		file.write((char*)std::data(locForms), std::size(locForms) * sizeof(Form));
+		file.write(std::data(stringPool), std::size(stringPool));
+
+		return true;
+	}
 }
